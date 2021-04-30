f25485b19255e85457f1e78d253df7a5f3e49a01
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-04-30 12:05:39 -0300

    Wdio validation test working correctly

--- test/specs/example1.js
+++ test/specs/example1.js
@@ -1,18 +1,36 @@
 
-describe('Wikipedia Search', () => {
+describe('Google Search: Wikipedia', () => {
 
-    beforeAll('open browser', () => {
+    beforeAll('Open browser', () => {
         return browser.url('http://google.com');
     });
 
-    it('can handle commands synchronously', () => {
-        let value = $('#input');
-        expect(value).toExist();
+    it('Finding search bar in DOM', () => {
+        let searchInput = $('form div:nth-child(1) div.A8SBwf div.RNNXgb div div.a4bIc input');
+        expect(searchInput).toExist();
     });
 
-    // it('title', () => {
-    //     browser.pause(2000);
-    //     expect(browser.getTitle()).toMatch('Wikipedia');
-    // });
+    it('Searching for \'wikipedia\'', () => {
+        let searchInput = $('form div:nth-child(1) div.A8SBwf div.RNNXgb div div.a4bIc input');
+        searchInput.addValue('wikipedia');
+
+        browser.keys('Enter');
+    });
+
+    it('Entering Wikipedia\'s homepage', () => {
+        let wikiPage = $('#rso div:nth-child(1) div:nth-child(1) div div.tF2Cxc div.yuRUbf a');
+        expect(wikiPage).toExist();
+        wikiPage.click();
+    });
+
+    it('Validating Wikipedia\'s URL', () => {
+        browser.pause(1000);
+        expect(browser.getUrl()).toMatch('https://es.wikipedia.org/wiki/Wikipedia:Portada');
+    });
+
+    it('Validating page\'s title', () => {
+        browser.pause(1000);
+        expect(browser.getTitle()).toMatch('Wikipedia, la enciclopedia libre');
+    });
 
 });
